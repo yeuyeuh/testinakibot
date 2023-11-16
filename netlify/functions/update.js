ea2535bd3b80e79d54b0343e643cce0d5c06772f
yeuyeuh <icerveramarzal@gmail.com>
2023-11-16 11:40:10 +0100

    schedule5

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -4,15 +4,17 @@ const sendMessage = require("../../sendMessage");
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
   //await sendMessage(message.chat.id, "I got your message!");
-  //setInterval(sendMessage(message.chat.id, "I got your message!"), 1200000);
+  setInterval(sendMessage(message.chat.id, "I got your message!"), 20000);
 
+  /*
   // Utilisez une fonction de rappel pour setInterval
   setInterval(() => {
     sendMessage(message.chat.id, "I got your message!")
       .catch((error) => {
         console.error('Erreur lors de l\'envoi du message:', error.message || error);
       });
-  }, 10000);
+  }, 30000);
+  */
 
   
   return { statusCode: 200 };
