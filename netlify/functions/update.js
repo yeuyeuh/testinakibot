76986b7823761e0979e888b7c7a8370e5c04f8c1
yeuyeuh <icerveramarzal@gmail.com>
2023-11-16 11:20:41 +0100

    schedule2

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -3,12 +3,8 @@ const sendMessage = require("../../sendMessage");
 
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
-  await sendMessage(message.chat.id, "I got your message!");
+  //await sendMessage(message.chat.id, "I got your message!");
+  setInterval(sendMessage(message.chat.id, "I got your message!"), 10000);
   
-  // Déclencher la fonction toutes les minutes
-  setInterval(() => {
-    sendMessage(message.chat.id, "I got your message!");
-  }, 10 * 1000); // 1 minute en millisecondes
-
   return { statusCode: 200 };
 };
