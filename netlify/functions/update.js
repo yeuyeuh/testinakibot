d2d6b0e1d7c132ca803c51d13446b055ea690cab
yeuyeuh <icerveramarzal@gmail.com>
2023-11-16 10:33:56 +0100

    refacto

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,14 +1,8 @@
-const axios = require("axios").default;
-
+const sendMessage = require("../../sendMessage");
 
 
 exports.handler = async (event) => {
-  console.log("Received an update from Telegram!", event.body);
-
-  await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-    chat_id: JSON.parse(event.body).message.chat.id,
-    text: "I got your message!",
-  });
-  
+  const { message } = JSON.parse(event.body);
+  await sendMessage(message.chat.id, "I got your message!");
   return { statusCode: 200 };
 };
