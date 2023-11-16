0cf76375c203181d465b08f09a2563edd0476692
yeuyeuh <icerveramarzal@gmail.com>
2023-11-16 11:26:06 +0100

    schedule3

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -4,7 +4,7 @@ const sendMessage = require("../../sendMessage");
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
   //await sendMessage(message.chat.id, "I got your message!");
-  setInterval(sendMessage(message.chat.id, "I got your message!"), 10000);
+  setInterval(sendMessage(message.chat.id, "I got your message!"), 1200000);
   
   return { statusCode: 200 };
 };
