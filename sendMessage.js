dcc7d6d8863abe6e21b6fe05a880cd05dcba0bc3
yeuyeuh <icerveramarzal@gmail.com>
2023-11-16 21:57:52 +0100

    why2endtry2yeuhyeutruefinish2

--- sendMessage.js
+++ sendMessage.js
@@ -13,7 +13,7 @@ module.exports = async (chat_id, text) => {
     } else {
       await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
         chat_id,
-        text: "*Nothing for the moment but searching...*",
+        "*Nothing for the moment but searching...*",
         parse_mode:Markdown,
         disable_web_page_preview: true,
       });
