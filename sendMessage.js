ed8b79ece20b74b9451c0946d3bed1a357f05c6f
yeuyeuh <icerveramarzal@gmail.com>
2023-11-16 21:20:41 +0100

    why2endtry2!=

--- sendMessage.js
+++ sendMessage.js
@@ -3,7 +3,7 @@ const axios = require("axios");//.default;
 module.exports = async (chat_id, text) => {
   console.log("text : ", text);
   try {
-    if (text !== "nothing") {
+    if (text != "nothing") {
       await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
         chat_id,
         text,
