2a9790b2de73f6ed60f643e034042b234fda98f7
yeuyeuh <icerveramarzal@gmail.com>
2023-11-16 11:44:27 +0100

    schedule6

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -4,8 +4,9 @@ const sendMessage = require("../../sendMessage");
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
   //await sendMessage(message.chat.id, "I got your message!");
-  setInterval(sendMessage(message.chat.id, "I got your message!"), 20000);
+  //setInterval(sendMessage(message.chat.id, "I got your message!"), 20000);
 
+  setInterval(() => {sendMessage(message.chat.id, "I got your message!")}, 20000);
   /*
   // Utilisez une fonction de rappel pour setInterval
   setInterval(() => {
